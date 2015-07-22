2eea17d73bf7127fb024a7e2dc541010a1ab344d
raptattention <klaw1794@gmail.com>
2015-07-22 15:54:59 +0800

    Creating highchart classes

---
+++ js/application.js
@@ -0,0 +1,54 @@
+'use strict';
+
+$(document).ready(function(){
+	var HighCharts = function(){
+    this.graphData = [];
+	};
+
+	HighCharts.prototype.makeAjaxRequest = function(){
+		$.ajax({
+      context: this,
+			type: 'GET',
+			url: 'https://www.quandl.com/api/v1/datasets/BTS_MM/RETAILGAS.json?auth_token=E6kNzExHjay2DNP8pKvB',
+			success: function(response){
+				var items = response.data;
+        var item;
+        for (var i=0; i < items.length; i++){
+          item = items[i];
+          this.graphData.push({
+            x: new Date (item[0]),
+            y: item[1]
+          });
+        }
+
+        console.log(this.graphData);
+        this.graph();
+			}
+		});
+	};
+
+  HighCharts.prototype.graph = function(){
+    var highchartConfig = {
+      title: {
+        text: "Average retail gas prices"
+      },
+      subtitle: {
+        text: "Bureau of Transportation Statistics (Multimodal)"
+      },
+      xAxis: {
+        text: 'datetime'
+      },
+      series: [
+        {
+          name: "US",
+          data: this.graphData.reverse()
+        }
+      ]
+    };
+
+    $('#chart').highcharts(highchartConfig);
+  } 
+
+  var chart =new HighCharts();
+  chart.makeAjaxRequest();
+});
